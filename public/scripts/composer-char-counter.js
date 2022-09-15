717bc26c82d079ade6193e6f686cbc38a3f2f01f
Ruanruanx <zxin7@uwo.ca>
2022-09-15 14:29:31 +0000

    Changed keyup to input

--- public/scripts/composer-char-counter.js
+++ public/scripts/composer-char-counter.js
@@ -2,8 +2,7 @@ $(document).ready(function() {
   // --- our code goes here ---
   console.log("ready");
   $('.counter')[0].innerText = 140;
-
-  $("textarea").keyup(function() {
+  $("textarea").on('input',function() {
     let len = $(this).val().length;
     let leftLen = 140 - len;
     let count = $(this).parent().find('.counter');
